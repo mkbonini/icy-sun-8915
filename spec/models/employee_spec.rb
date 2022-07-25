4eeaf7ac62fa2ec5480307329e78dbac7d30237a
mkbonini <mkbonini@gmail.com>
2022-07-25 09:22:36 -0600

    Test: add data validation to tests

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe Employee, type: :model do
     it { should have_many(:tickets)}
     it { should have_many(:tickets).through(:employee_tickets)}
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:level) }
+    it { should validate_numericality_of(:level) }
+  end
 end
--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe Ticket, type: :model do
         it { should have_many(:employees)}
         it { should have_many(:employees).through(:employee_tickets)}
     end
+
+    describe 'validations' do
+        it { should validate_presence_of(:subject) }
+        it { should validate_presence_of(:age) }
+        it { should validate_numericality_of(:age) }
+      end
 end
