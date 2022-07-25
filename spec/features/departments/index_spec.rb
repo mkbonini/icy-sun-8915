4c0d237979a009dcccd81d56c2ab2d2e4cbdcd06
mkbonini <mkbonini@gmail.com>
2022-07-25 10:09:29 -0600

    Feat: department index page w/ employee names

--- spec/features/departments/index_spec.rb
+++ spec/features/departments/index_spec.rb
@@ -13,12 +13,12 @@ RSpec.describe 'departments index page' do
             expect(page).to have_content("sub-basement")
         end
 
-        within "#dept-#{dept_1.id}" do
+        within "#dept-#{dept_2.id}" do
             expect(page).to have_content("HR")
             expect(page).to have_content("3")
         end
 
-        within "#dept-#{dept_1.id}" do
+        within "#dept-#{dept_3.id}" do
             expect(page).to have_content("C Suite")
             expect(page).to have_content("Penthouse")
         end
@@ -29,9 +29,9 @@ RSpec.describe 'departments index page' do
         dept_2 = Department.create!(name: 'HR', floor: '3')
         dept_3 = Department.create!(name: 'C Suite', floor: 'Penthouse')
 
-        emp_1 = Employee.create!(name: 'Mark', level: 5, department_id: dept_1)
-        emp_2 = Employee.create!(name: 'John', level: 1, department_id: dept_1)
-        emp_3 = Employee.create!(name: 'Mary', level: 3, department_id: dept_2)
+        emp_1 = Employee.create!(name: 'Mark', level: 5, department_id: dept_1.id)
+        emp_2 = Employee.create!(name: 'John', level: 1, department_id: dept_1.id)
+        emp_3 = Employee.create!(name: 'Mary', level: 3, department_id: dept_2.id)
 
         visit '/departments'
 
@@ -41,13 +41,13 @@ RSpec.describe 'departments index page' do
             expect(page).to_not have_content("Mary")
         end
 
-        within "#dept-#{dept_1.id}" do
+        within "#dept-#{dept_2.id}" do
             expect(page).to_not have_content("Mark")
             expect(page).to_not have_content("John")
             expect(page).to have_content("Mary")
         end
 
-        within "#dept-#{dept_1.id}" do
+        within "#dept-#{dept_3.id}" do
             expect(page).to_not have_content("Mark")
             expect(page).to_not have_content("John")
             expect(page).to_not have_content("Mary")
